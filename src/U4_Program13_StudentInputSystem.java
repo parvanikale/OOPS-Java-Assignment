3194c4751cceaba3cc28e2a15ad3028c08ae19c5
parvanikale <parvaniskale@gmail.com>
2026-03-26 12:28:18 +0530

    Updated Program13 Student Input System

--- src/U4_Program13_StudentInputSystem.java
+++ src/U4_Program13_StudentInputSystem.java
@@ -1,18 +1,13 @@
-import java.util.Scanner;
-import java.util.InputMismatchException;
+import java.util.*;
 
-public class U4_Program13_StudentInputSystem {
-
+class StudentInputSystem {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
 
-        try {
             System.out.print("Enter student age: ");
             int age = sc.nextInt();
 
-            // Example ArithmeticException (optional logic)
-            int check = age / 1;
 
             System.out.println("Student age is: " + age);
 
@@ -21,7 +16,5 @@ public class U4_Program13_StudentInputSystem {
         } catch (ArithmeticException e) {
             System.out.println("Invalid input");
         }
-
-        sc.close();
     }
 }
